683b583e6025c411f42757ccd10492594124c296
flan the dev <flanthedev@gmail.com>
2017-08-13 20:37:58 +0800

    finish UI, update readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 JS project to practice OOP
+<br><a href="https://flanthedev.github.io/js_personality_test/">view on GitHub Pages</a>
--- js/script.js
+++ js/script.js
@@ -26,3 +26,60 @@ Test.prototype.currentQuestion = function() {
 Test.prototype.isComplete = function() {
   return this.questionIndex >= this.questions.length;
 };
+
+//create test and display
+var questions = [
+  new Question('Do you prefer kittens or cockroaches?', ['kittens', 'cockroaches'], 'kittens'),
+  new Question('Do you prefer puppies or mosquitoes?', ['puppies', 'mosquitoes'], 'puppies')
+]
+var test = new Test(questions);
+
+//TestUI
+var TestUI = {
+  displayNext: function() {
+    if ( test.isComplete() ) {
+      this.displayScore();
+    } else {
+      this.displayQuestion();
+      this.displayChoices();
+      this.displayProgress();
+    }
+  },
+
+  displayProgress: function() {
+    let currentQuestionNum = test.questionIndex + 1;
+    this.findIdInsertHTML('progress', `Questions ${currentQuestionNum} of ${test.questions.length}`);
+  },
+
+  displayQuestion: function() {
+    this.findIdInsertHTML('question', test.currentQuestion().text);
+  },
+
+  displayChoices: function() {
+    let choices = test.currentQuestion().choices;
+    for (var i = 0; i < choices.length; i++) {
+      this.findIdInsertHTML('choice' + i, choices[i]);
+      this.guessHandler('guess' + i, choices[i]);
+    }
+  },
+
+  displayScore: function() {
+    this.findIdInsertHTML('test', `You've completed the test. Your score is ${test.score}.`);
+  },
+
+  findIdInsertHTML: function(id, text) {
+    let element = document.getElementById(id);
+    element.innerHTML = text;
+  },
+
+  guessHandler: function(id, guess) {
+    let button = document.getElementById(id);
+    button.onclick = function() {
+      test.guess(guess);
+      TestUI.displayNext();
+    }
+  }
+
+}
+
+TestUI.displayNext();
